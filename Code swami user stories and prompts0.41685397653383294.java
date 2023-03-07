062a3ff139f32683259cbd7057c0ef8ddf403b95
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-07 11:02:09 +0530

    initial commit

---
+++ Code swami user stories and prompts0.41685397653383294.java
@@ -0,0 +1,18 @@
+
+Spring Boot Application
+
+The following is a Spring Boot application for code generation for Java/.Net/Python/Node, selecting patterns, designs, Jira project details, and GitHub settings for publishing code to GitHub. 
+
+The application will include a controller, service, and repository classes that will handle user input and generate the code accordingly.
+
+Controller
+
+The controller class will handle user input and process the data to generate the code according to the user’s selections. It will handle the selection of code generation for Java/.Net/Python/Node, selection of patterns, designs, Jira project details, and GitHub settings.
+
+Service
+
+The service class will handle the logic for generating the code. It will take the user input from the controller and generate the code according to the user’s selections. It will also handle the integration of the code with the Jira and GitHub settings for version control.
+
+Repository
+
+The repository class will handle the data storage and retrieval of the code. It will store the code generated from the service and retrieve it when requested. It will also handle the version control of the code, ensuring that the latest version of the code is always retrieved.
